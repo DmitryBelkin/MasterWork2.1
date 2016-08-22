e5a94b5a88735ff8ceef192a553bf4bf5d32c84a
Dmitry Belkin <belckin.d@yandex.ru>
2016-08-23 00:51:32 +0700

    cf 14

--- src/FEM.h
+++ src/FEM.h
@@ -33,27 +33,27 @@ public:
 	vector < vector < double > >           m_C;
 
 	// methods
-	void Input();
-	void InputMesh();
-	void InputSlauParameters();
-	void InputElastityParameters();
-	void SolveProblem();
+	void   Input();
+	void   InputMesh();
+	void   InputSlauParameters();
+	void   InputElastityParameters();
 
-	void SetDefault();
-	void GenerateMatrixProfle();
-	void CreateGlobalMatrixAndRightPart();
-	void GenerateLocalStiffnessMatrix(int numNvtr, vector <vector <double> > &KLocal) const;
-	void GenerateLocalBlockK(int i, int j, int numNvtr, vector <vector <double> > &LocalBlockK) const;
+	void   SolveProblem();
+	void   SetDefault();
+	void   GenerateMatrixProfle();
+	void   CreateGlobalMatrixAndRightPart();
+	void   GenerateLocalStiffnessMatrix(int numNvtr, vector <vector <double> > &KLocal) const;
+	void   GenerateLocalBlockK(int i, int j, int numNvtr, vector <vector <double> > &LocalBlockK) const;
 	double VolumeOfParallelepiped(int numNvtr) const;
-	bool PointBelongsToArea(double x, double y, double z) const;
-	bool PointBelongsToParallelepiped(double x, double y, double z, int numNvtr) const;
-	void AddLocalToGlobal(vector <int> &mtrx, vector <vector <double> > &K, vector <double> &b);
-	void AddBlockToLocalStiffnessMatrix(int numi, int numj, vector <vector <double> > &KLocal , vector <vector <double> > &LocalBlockK) const;
-	void AddNvtr(vector <int> &mtr);
-	void AddDegree(vector <int> &mtr);
-	void PrintFigure();
-	void BoundaryConditions();
-	void Boundary_1(int num, double Ug);
+	bool   PointBelongsToArea(double x, double y, double z) const;
+	bool   PointBelongsToParallelepiped(double x, double y, double z, int numNvtr) const;
+	void   AddLocalToGlobal(vector <int> &mtrx, vector <vector <double> > &K, vector <double> &b);
+	void   AddBlockToLocalStiffnessMatrix(int numi, int numj, vector <vector <double> > &KLocal , vector <vector <double> > &LocalBlockK) const;
+	void   AddNvtr(vector <int> &mtr);
+	void   AddDegree(vector <int> &mtr);
+	void   PrintFigure();
+	void   BoundaryConditions();
+	void   Boundary_1(int num, double Ug);
 	double GetUg(double x, double y, double z);
 	double GetTraction_1(double x, double y, double z);
 	double GetTraction_2(double x, double y, double z);
@@ -65,7 +65,7 @@ public:
 	double DerivativeZ(int num, int numNvtr) const;
 	double ThreeLinearFunctionValue(int k, double x, double y, double z, int numNvtr) const;
 	double ThreeLinearFunctionDerivative(int k, int numNvtr) const;
-	void TransformMeshAfterDisplacement();
-	void GenerateMeshForCheck();
-	void SetBordersOfCheckMesh(double xLeftCheckMesh, double xRightCheckMesh, double yLeftCheckMesh, double yRightCheckMesh, double zLeftCheckMesh, double zRightCheckMesh);
+	void   TransformMeshAfterDisplacement();
+	void   GenerateMeshForCheck();
+	void   SetBordersOfCheckMesh(double xLeftCheckMesh, double xRightCheckMesh, double yLeftCheckMesh, double yRightCheckMesh, double zLeftCheckMesh, double zRightCheckMesh);
 };
